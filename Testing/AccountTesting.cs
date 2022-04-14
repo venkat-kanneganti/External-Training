0239bc32ff4051b60a0d8ba4b7e937d9cfd80936
venkat-kanneganti <sivasaikanneganti@gmail.com>
2022-04-14 16:56:28 +0530

    Add project files.

---
+++ Testing/AccountTesting.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit_Testing;
+
+namespace Testing
+{
+    [TestFixture]
+    public class AccountTesting
+    {
+        Account acc;
+        [SetUp]
+
+        public void TestSetup()
+        {
+            acc = new Account("I-34567");
+        }
+
+        [Test]
+
+        public void TestDepositMethod()
+        {
+            //ask
+            acc.Deposit(800);
+            //assert
+            Assert.AreEqual(1800, acc.CheckBalance());
+        }
+        public void TestWithdraw()
+        {
+            //ask
+            acc.WithDraw(2000);
+        }
+        [Test]
+        public void TestWithDrawException()
+        {
+            Assert.Throws<Exception>(TestWithdraw);
+        }
+        [Test]
+        public void JustChecking_Balance()
+        {
+            Assert.AreNotEqual(1800, acc.CheckBalance());
+        }
+
+    }
+}
